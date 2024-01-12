470b016939262d8ac1f27b2c0a25c3629c36a7ea
doualecl <clement.douale@etu.utc.fr>
2024-01-12 15:11:17 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example: Toggling a mobile menu
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('nav-menu').classList.toggle('active');
+});
